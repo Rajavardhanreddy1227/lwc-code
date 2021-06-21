0c615c23f109df5ac24208db5c9bb6b0f62abb31
Rajavardhan Reddy Marikanti <rajavardhanreddy666@gmail.com>
2021-06-21 19:57:28 +0530

    create the ContactCreator lwc

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -1,3 +1,12 @@
+/*import { LightningElement , api, track} from 'lwc';
+
+export default class LWBDemoComponent extends LightningElement {
+@api recordId;
+@api objectApiName;
+@track fields = ['Name', 'Title', 'Phone', 'Email'];
+}
+*/
+
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
